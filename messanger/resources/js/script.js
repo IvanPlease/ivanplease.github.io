226ec74a578637c9466104eb7663a91e1e8322d6
QueIvan <szewczykjan2@gmail.com>
2020-09-20 12:43:27 +0200

    Friend search implementation

--- messanger/resources/js/script.js
+++ messanger/resources/js/script.js
@@ -1,26 +1,14 @@
 var cluster_check = false;
+var api_host = "https://delta-communicator.herokuapp.com/";
+var api_local = "http://localhost:8080";
 
 $(document).ready(function(){
     var box = [{userMsg: true, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in volutpat ligula. Etiam tempus quis massa rutrum lobortis. Integer ut scelerisque ex. Praesent in magna sed tellus viverra auctor. Morbi quis nisl tempor, mattis diam quis, aliquam magna. Proin eget pulvinar lectus. In non nisi sed ex molestie blandit. Curabitur dignissim tellus quis arcu cursus sagittis. Ut neque velit, pulvinar quis neque sit amet, porta feugiat diam. Morbi euismod sit amet urna et laoreet. Ut lacus justo, sodales vel volutpat sit amet, auctor id quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce faucibus condimentum auctor."},{userMsg: true, content: "message"},{userMsg: true, content: "message"},{userMsg: false, content: "message #2"}, {userMsg: false, content: "message #3"}, {userMsg: false, content: "message #4"}];
-    box.forEach(msg => {
-        addMessage(msg)
-    });
-    box.forEach(msg => {
-        addMessage(msg)
-    });
-    box.forEach(msg => {
-        addMessage(msg)
-    });
     $("#friendSearch").on('click', function(){
         var element = $("#friendSearch").parent().find("input");
-        var searchTerm = element.val().toLowerCase();
+        var searchTerm = element.val();
         var ajaxValue = {searchType: 0, searchValue: searchTerm};
-        if(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/g.test(searchTerm)){
-            ajaxValue.searchType = 2;
-        }else if(/^\b[a-zA-Z]+\b\s\b[a-zA-Z]+\b$/g.test(searchTerm) || /\b[a-zA-Z]+\b$/g.test(searchTerm)){
-            ajaxValue.searchType = 1;
-        }
-        element.val('');
+        searchLogic(ajaxValue);
     });
     $("#sendMessage").on('click', function(){
         var element = $("#sendMessage").parent().find("input");
@@ -32,13 +20,7 @@ $(document).ready(function(){
         }
         element.val('');
     });
-    $.ajax({
-        method: "GET",
-        url: "https://delta-communicator.herokuapp.com/v1/users/1",
-        success: function(data){
-            console.log(data);
-        }
-    })
+    $("#addFriendModal").on('hidden.bs.modal', clearSearchBox);
 });
 
 function addMessage(msg){ //on new message recieved while online
@@ -67,4 +49,36 @@ function addMessage(msg){ //on new message recieved while online
         }
     }
     $(messageBoxTag).prepend(messageBlop.replace("${messageContent}", msg.content).replace("${messageAuthor}", author).replace("${messageSide}", side).replace("${clusterContent}", ""));
+}
+
+function clearSearchBox(){
+    $("#searchBoxReturn").children().not(":first").remove();
+    $("#searchBoxReturn").children().find(".friend-name-input-box>input").val('');
+}
+
+function searchLogic(ajaxValue){
+    var searchBox = '<div class="d-flex flex-row searchBackground mt-2"><div class="d-inline-flex p-2 w-100"><div class="image-wrap"><img src="${dataPath}" alt="${dataAlt}"/></div><div class="ml-3 position-relative w-100"><span class="searchData w-100">${dataNameAndSurname}</span></div></div></div>'
+    if(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/g.test(ajaxValue.searchValue)){
+        ajaxValue.searchType = 2;
+    }else if(/^\b[a-zA-Z]+\b\s\b[a-zA-Z]+\b$/g.test(ajaxValue.searchValue)){
+        ajaxValue.searchType = 1;
+    }else if(/\b[a-zA-Z]+\b$/g.test(ajaxValue.searchValue)){
+        ajaxValue.searchType = 0;
+    }
+    console.log(ajaxValue.searchValue);
+    $.ajax({
+        method: "GET",
+        url: api_local + "/v1/users",
+        dataType: "json",
+        data: ajaxValue,
+        success: function(data){
+            data.forEach(user => {
+                var name = user.firstname + " " + user.lastname;
+                $("#searchBoxReturn").append(searchBox.replace("${dataPath}", user.profilePic.filePath).replace("${dataAlt}", user.profilePic.fileName).replace("${dataNameAndSurname}", name));
+            })
+        },
+        error: function(data){
+            console.log(data);
+        }
+    })
 }
